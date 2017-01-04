720d6c4d546eec8c9ffa3b5833ee45ac584b0948
singha <kashmir@123>
2017-01-04 23:24:05 +0530

    remove comma

--- environments/production/modules/profile/manifests/rabbitmq_profile.pp
+++ environments/production/modules/profile/manifests/rabbitmq_profile.pp
@@ -34,7 +34,7 @@ class profile::rabbitmq_profile {
     notify { 'Enabling Managemtn UI' : }
 
     exec { 'Enable Plugin':
-      command   => "$(cd '${rabbitmq_path}'; .\\rabbitmq-plugins.bat enable rabbitmq_management)",
+      command   => "$(cd ${rabbitmq_path}; .\\rabbitmq-plugins.bat enable rabbitmq_management)",
       provider  => powershell,
       logoutput => true,
     }
@@ -42,7 +42,7 @@ class profile::rabbitmq_profile {
     notify { 'Creating user': }
 
     exec { 'create user':
-      command   => "$(cd '${rabbitmq_path}'; .\\rabbitmqctl.bat add_user liquid arka.com@2015; .\\rabbitmqctl.bat set_user_tags liquid administrator; .\\rabbitmqctl.bat set_permissions -p / liquid '^liquid-.*' '.*' '.*')",
+      command   => "$(cd ${rabbitmq_path}; .\\rabbitmqctl.bat add_user liquid arka.com@2015; .\\rabbitmqctl.bat set_user_tags liquid administrator; .\\rabbitmqctl.bat set_permissions -p / liquid '^liquid-.*' '.*' '.*')",
       provider  => powershell,
       logoutput => true,
     }
