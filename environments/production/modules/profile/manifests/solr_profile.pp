aa7dc4f72d7f6d0105f0b9fb831ec85d19b02c30
singha <kashmir@123>
2017-01-20 15:34:05 +0530

    delete file

--- environments/production/modules/profile/manifests/solr_profile.pp
+++ environments/production/modules/profile/manifests/solr_profile.pp
@@ -27,22 +27,35 @@ class profile::solr_profile{
       destination_directory => $dest_path
     }
 
-
     if validate_absolute_path('D:\Solr\solr-6.2.1') {
       notify {"Unzipping Solr":}
-
       exec { 'Unzipping  Solr':
         command   => "$(Add-Type -assembly 'system.io.compression.filesystem'; [io.compression.zipfile]::ExtractToDirectory('${dest_path}\\solr-6.2.1.zip','D:\Solr'))",
         provider  => powershell,
         logoutput => true
       }
-    }
 
-   notify { 'Installing Solr': }
-   exec { 'Installing Solr':
-      command   => "$('.\\${dest_path}\\nssm.exe install SolrServer D:\Solr\solr-6.2.1\bin\solr.cmd start -p -f 8983; nssm start SolrServer')",
-      provider  => powershell,
-      logoutput => true,
+      notify { 'Installing Solr': }
+      exec { 'Installing Solr':
+        command   => "$('.\\${dest_path}\\nssm.exe install SolrServer D:\Solr\solr-6.2.1\bin\solr.cmd start -p -f 8983; nssm start SolrServer')",
+        provider  => powershell,
+        logoutput => true,
+      }
+
+      exec { 'Checking Solr is Start':
+        command   => "$(if(Get-Service SolrServer -ErrorAction SilentlyContinue) { '${issolr_installed} = true'; } else { '${issolr_installed} = false'; })",
+        provider  => powershell,
+        logoutput => true,
+      }
+
+      if(isolr_installed) {
+        exec { 'Create the Solr Core': 
+          command   => "$()",
+          provider  => powershell,
+          logoutput => true        
+        }
+      }
+
     }
 
   }
